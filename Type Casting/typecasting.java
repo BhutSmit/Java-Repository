83bdff04f0e41633986f49b707e9a24e34a538c6
Smit Bhut <93771507+BhutSmit@users.noreply.github.com>
2022-02-13 11:58:55 +0530

    Update typecasting.java

--- Type Casting/typecasting.java
+++ Type Casting/typecasting.java
@@ -11,18 +11,30 @@ public class typecasting{
 }
 
 	
-	// ✔ In This Typecasingc(Narrowing TyppeCasting). 
+	//   ✔   In This Typecasingc(Narrowing TyppeCasting). 
 
-	// ✔ Here We Convert Integer ----->  Byte.
+	//   ✔   Here We Convert Integer ----->  Byte.
 
-	// ✔ So This Type Casting Is Perform Manually.
+	//   ✔   So This Type Casting Is Perform Manually.
 
-	// ✔ The Range Of Byte Is -128 To  127.
+	//   ✔   The Range Of Byte Is -128 To  127.
 
-	// ✔ So When We Add A Number Out Of A Range Here A Clycle Of A Number Is Start.
+	//   ✔   So When We Add A Number Out Of A Range Here A Clycle Of A Number Is Start.
 
-	// ✔ (-128  -127  -126  -125  -124  -123  -122  -121 -120 .............  127)  128  129  130  131  132  133 134..
+	//   ✔   (-128  -127  -126  -125  -124  -123  -122  -121 -120 .............  127)  128  129  130  131  132  133 134..
 
-	// ✔  When We Add A Number Like 129  So Process Is As Per Below : 
+	//   ✔    When We Add A Number Like 129  So Process Is As Per Below : 
 
-	//  	129 Is is greter than its range so here a cycle is start and print -127
+	//  ---> 129 Is is greter than its range so here a cycle is start and print -127
+
+	//		Input -----> Output
+	
+        //		 128 ------> -128
+
+	//		 129 ------> -127
+
+	//		 130 ------> -126 
+
+	//		 131 ------> -125
+
+			
